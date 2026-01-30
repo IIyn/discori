01eab5f18e70f7e88cd7e28c8f93b675f4567830
IIyn <yassinam.pro@gmail.com>
2026-01-30 11:53:33 +0100

    updated dev and updated readme

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,19 +3,11 @@
     windows_subsystem = "windows"
 )]
 
-// Tauri imports
-use std::fs;
 use tauri::{
     menu::{Menu, MenuItem},
     tray::{TrayIconBuilder, TrayIconEvent},
     Manager,
 };
-
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -96,7 +88,7 @@ pub fn run() {
         //     }
         //     _ => {}
         // })
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
